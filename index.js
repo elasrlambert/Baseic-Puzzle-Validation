82dae19de5761a09b5d81c7d8d75c05c1e09f681
elasrlambert <84452882+elasrlambert@users.noreply.github.com>
2022-10-24 12:47:44 +0800

    Update index.js
    add some chinese and english notes.

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
 window.onload = function() {
-    console.log('JS引入成功！');
+    // console.log('JS引入成功！');
 
-    // 初始化
+    // 初始化 initialization
     var mouseTrigger = document.getElementsByClassName('trigger-box')[0]
     var moveTrigger = document.getElementsByClassName('triggered-box')[0]
     var targetTrigger = document.getElementsByClassName('target-box')[0]
-    var bottom = document.getElementsByClassName('bottom')[0]
+    var movingArea = document.getElementsByClassName('bottom')[0]
 
 
 
-    //鼠标事件有一个回调函数，它的参数是e（也可以改成其它名字）
+    //Add event listener for 'mouseTrigger' and 'movingArea'
+    //为 'mouseTrigger' 和 'movingArea' 添加事件监听器。
     mouseTrigger.addEventListener('mousedown',function () {
-        bottom.addEventListener('mousemove',function(e){
+        movingArea.addEventListener('mousemove',function(e){
             boxMove(e)
             
         })
     })
-    mouseTrigger.addEventListener('mouseup',function (e) {
-        boxMove(e)
+    // Add event mouseup listener for mouseTrigger,
+    // do deferent event depend on the certain limit range.
+    // 计算误差，根据误差是否在一定的范围内来执行不同的行为 
+    mouseTrigger.addEventListener('mouseup',function () {
+
         var difference = parseInt(moveTrigger.style.left) - parseInt(targetTrigger.style.left);
         console.log('误差为 ==> ',difference);
         if ( difference <= 25 && difference >= -25 ) {
-            alert('成功！')
+            alert('succeed !')
             history.go(0)
         }else{
-            alert('失败！')
+            alert('fail !')
             history.go(0)
         }
     })
@@ -32,25 +36,26 @@ window.onload = function() {
         if (e) {
 
             mouseTrigger.style = 'position: absolute;'
+            // "(-50)" is the half of the box's with
+            // "(-50)" 是盒子的一半宽度，计算出来是盒子中心到左边的距离。
             mouseTrigger.style.left = e.offsetX + (-50) + 'px';
 
             moveTrigger.style = 'position: absolute;'
             moveTrigger.style.left = e.offsetX + (-50) + 'px';
 
-            // console.log('鼠标的位置',e.offsetX);
-            //这是行内样式，所以外联的css样式获取不到。
-            // console.log('盒子的位置',mouseTrigger.style.left);
             
         }
     }
-    //生成随机范围
+    // Generate random positions in the range for the target box
+    // 为目标盒子生成范围内的随机位置
     function randomLocation(min,max){
             var Range = max - min;
             var Rand = Math.random();
             var num = min + Math.round(Rand * Range); //四舍五入
-            console.log('随机位置 ==> ',num);
+            // console.log('随机位置 ==> ',num);
             targetTrigger.style = 'position: absolute;'
             targetTrigger.style.left = num + 'px'
     }
     randomLocation(100,500)
+    console.warn('喜欢请给这个项目一颗星星~','\n','Plz give it a star if you like this project~');
 }
